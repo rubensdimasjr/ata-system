261aa00631bdfeae9bbc8e4f46c5ca3156424068
Rubens Júnior <rubens.jr107@gmail.com>
2022-05-18 22:47:26 -0300

    updated style of title and image

--- index.php
+++ index.php
@@ -8,17 +8,29 @@
   <title>Ata de Reunião</title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
+  <!-- Style Index -->
+  <link rel="stylesheet" href="./assets/css/index.css">
+
   <script src="https://kit.fontawesome.com/cf4fb9e680.js" crossorigin="anonymous"></script>
   <script type="text/javascript" src="./assets/js/jquery.js"></script>
 </head>
 
 <body>
   <div class="container">
-    <div class="row mt-5 justify-content-center">
+    <div class="row justify-content-center">
+      <div class="row">
+        <div class="col-lg-6 col-sm-12 justify-content-center d-flex titulo ">
+          <h1 class="fw-bold">SISTEMA DE EMISSÃO DE ATAS</h1>
+        </div>
+        <div class="col-lg-6 col-sm-12 text-center">
+          <img src="./assets/images/Horizontal-Colorida.png" width="75%" class="img-fluid" alt="Logo">
+        </div>
+      </div>
       <div class="col-lg-8 col-sm-12 mb-sm-3">
         <div class="card mx-auto">
           <div class="card-header">
-            <h3 class="text-center">Formulário de Dados</h3>
+            <h3 class="text-center fw-bold text-white">Formulário de Dados</h3>
           </div>
           <div class="card-body">
             <form id="form1" method="post" action="./document.php">
@@ -32,7 +44,7 @@
 
               <div class="mb-3 d-flex justify-content-end">
                 <button class="btn btn-success botao">
-                  Add
+                  Adicionar
                 </button>
               </div>
               <div class="mb-3 lista">
